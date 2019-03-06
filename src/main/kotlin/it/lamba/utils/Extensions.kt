88952a5b18c16b687d62d98969dd32d3fbb8e058
Lamba <basti.lamberto@gmail.com>
2019-03-06 18:06:37 +0100

    Made extension functions suspending

--- src/main/kotlin/it/lamba/utils/Extensions.kt
+++ src/main/kotlin/it/lamba/utils/Extensions.kt
@@ -4,26 +4,26 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-fun Iterable<AbstractCoroutineWorker>.stopAll(wait: Boolean = false) =
-    map { GlobalScope.launch { it.stop(wait) } }.takeIf { wait }?.forEach { runBlocking { it.join() } } ?: Unit
+suspend fun Iterable<AbstractCoroutineWorker>.stopAll(wait: Boolean = false) =
+    map { GlobalScope.launch { it.stop(wait) } }.takeIf { wait }?.forEach { it.join() } ?: Unit
 
-fun Iterable<AbstractCoroutineWorker>.startAll(wait: Boolean = false) =
-    map { GlobalScope.launch { it.start(wait) } }.takeIf { wait }?.forEach { runBlocking { it.join() } } ?: Unit
+suspend fun Iterable<AbstractCoroutineWorker>.startAll(wait: Boolean = false) =
+    map { GlobalScope.launch { it.start(wait) } }.takeIf { wait }?.forEach { it.join() } ?: Unit
 
-fun Iterable<AbstractCoroutineWorker>.resetAll(wait: Boolean = false) =
-    map { GlobalScope.launch { it.reset(wait) } }.takeIf { wait }?.forEach { runBlocking { it.join() } } ?: Unit
+suspend fun Iterable<AbstractCoroutineWorker>.resetAll(wait: Boolean = false) =
+    map { GlobalScope.launch { it.reset(wait) } }.takeIf { wait }?.forEach { it.join() } ?: Unit
 
-fun Iterable<AbstractCoroutineWorker>.restartAll(wait: Boolean = false) =
-    map { GlobalScope.launch { it.restart(wait) } }.takeIf { wait }?.forEach { runBlocking { it.join() } } ?: Unit
+suspend fun Iterable<AbstractCoroutineWorker>.restartAll(wait: Boolean = false) =
+    map { GlobalScope.launch { it.restart(wait) } }.takeIf { wait }?.forEach { it.join() } ?: Unit
 
-fun Array<AbstractCoroutineWorker>.stopAll(wait: Boolean = false) =
-    map { GlobalScope.launch { it.stop(wait) } }.takeIf { wait }?.forEach { runBlocking { it.join() } } ?: Unit
+suspend fun Array<AbstractCoroutineWorker>.stopAll(wait: Boolean = false) =
+    map { GlobalScope.launch { it.stop(wait) } }.takeIf { wait }?.forEach { it.join() } ?: Unit
 
-fun Array<AbstractCoroutineWorker>.startAll(wait: Boolean = false) =
-    map { GlobalScope.launch { it.start(wait) } }.takeIf { wait }?.forEach { runBlocking { it.join() } } ?: Unit
+suspend fun Array<AbstractCoroutineWorker>.startAll(wait: Boolean = false) =
+    map { GlobalScope.launch { it.start(wait) } }.takeIf { wait }?.forEach { it.join() } ?: Unit
 
-fun Array<AbstractCoroutineWorker>.resetAll(wait: Boolean = false) =
-    map { GlobalScope.launch { it.reset(wait) } }.takeIf { wait }?.forEach { runBlocking { it.join() } } ?: Unit
+suspend fun Array<AbstractCoroutineWorker>.resetAll(wait: Boolean = false) =
+    map { GlobalScope.launch { it.reset(wait) } }.takeIf { wait }?.forEach { it.join() } ?: Unit
 
-fun Array<AbstractCoroutineWorker>.restartAll(wait: Boolean = false) =
-    map { GlobalScope.launch { it.restart(wait) } }.takeIf { wait }?.forEach { runBlocking { it.join() } } ?: Unit
+suspend fun Array<AbstractCoroutineWorker>.restartAll(wait: Boolean = false) =
+    map { GlobalScope.launch { it.restart(wait) } }.takeIf { wait }?.forEach { it.join() } ?: Unit
